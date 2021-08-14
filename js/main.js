e3b1c0aa618ba682ddcb43a6fa256818c22e819c
thuphung92@gmail.com <thuphung492@gmail.com>
2021-08-14 02:14:52 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const APIKEY = '5e6d915a8be4c7b572f373743a2c4e28';
+const kelvinToFarenheit = (K) => ((K-273.15)*1.8)+32;
+
+const city = document.querySelector('.loc');
+const icon = document.querySelector('.icon');
+const temp = document.querySelector('.temp-value p');
+const desc = document.querySelector('.temp-desc p');
+const minMax = document.querySelector('.min-max p');
+
+const input = document.querySelector('#input');
+console.log(input);
+const button = document.getElementById('button').addEventListener('click',getLocation);
+
+
+function getLocation (event) {
+    city = input.value; // need to fixed to get what typed in later
+    console.log(`City Name: ${city}`);
+    getWeather(city); // API call to get weather info
+}
+
+
+console.log(getWeather('westminster')) // assign Westminster to test data only
+const weather = {}
+async function getWeather(city) {
+    const result = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`)
+                    .then((response) => {let data = response.json(); return data})
+                    .then((data) => getInfo(data))
+                    .then(() => display())
+                    .catch((error) => {console.log(error)})
+        
+};
+
+// Show error messages
+
+function getInfo (data){
+    weather['temp'] = Math.round(kelvinToFarenheit(data.main.temp));
+    weather['tempMin'] = Math.round(kelvinToFarenheit(data.main.temp_min));
+    weather['tempMax'] = Math.round(kelvinToFarenheit(data.main.temp_max));
+    weather['city'] = data.name;
+    weather['icon'] = data.weather[0].icon;
+    weather['desc'] = data.weather[0].description
+};
+
+console.log(weather);
+
+const display = () => {
+    city.innerHTML = `${weather.city}`;
+    icon.innerHTML = `<img src="icons/${weather.icon}.png">`;
+    temp.innerHTML = `${weather.temp}°F`;
+    desc.innerHTML = `${weather.desc}`;
+    minMax.innerHTML = `H:${weather.tempMin}° <span>-</span> L:${weather.tempMax}°`;
+};
+
+
+
+
+
+
