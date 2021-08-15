a6693cb6be5c6b0c76b521a5c119d10f6fecb748
Thu Phung <85381962+thuphung92@users.noreply.github.com>
2021-08-15 00:21:53 -0700

    added date

--- js/main.js
+++ js/main.js
@@ -1,55 +1,53 @@
 const APIKEY = '5e6d915a8be4c7b572f373743a2c4e28';
 const kelvinToFarenheit = (K) => ((K-273.15)*1.8)+32;
+const today = new Date().toUTCString();
 
 const city = document.querySelector('.loc');
 const icon = document.querySelector('.icon');
 const temp = document.querySelector('.temp-value p');
 const desc = document.querySelector('.temp-desc p');
+const date = document.querySelector('.date');
 const minMax = document.querySelector('.min-max p');
 
-//const input = document.querySelector('#input');
-//console.log(input);
-
-const button = document.getElementById('button').addEventListener('click',console.log('clicked')); // just for testing if the button works, replace by getLocation later
+const button = document.getElementById('button').addEventListener('click', (event) => getLocation (event));
 
 function getLocation (event) {
-    let city = document.getElementsByName('city')[0].value; // need to fixed to get what typed in later
+    event.preventDefault();
+    const city = document.getElementsByName('city')[0].value;
     console.log(`City Name: ${city}`);
     getWeather(city); // API call to get weather info
 }
 
 
-console.log(getWeather('westminster')) // assign Westminster to test if the other functions work only
-const weather = {}
 function getWeather(city) {
-    const result = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`)
+    const result = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`, headers={Authorization: APIKEY})
                     .then((response) => {let data = response.json(); return data})
                     .then((data) => getInfo(data))
-                    .then(() => display())
-                    .catch((error) => {console.log(error)})
+                    .catch((error) => alert(error))
         
 };
 
-// Show error messages
 
+// Add info to weather list + display info
 function getInfo (data){
+    //get info
+    const weather = {}
     weather['temp'] = Math.round(kelvinToFarenheit(data.main.temp));
     weather['tempMin'] = Math.round(kelvinToFarenheit(data.main.temp_min));
     weather['tempMax'] = Math.round(kelvinToFarenheit(data.main.temp_max));
     weather['city'] = data.name;
     weather['icon'] = data.weather[0].icon;
     weather['desc'] = data.weather[0].description
-};
 
-console.log(weather);
-
-const display = () => {
+    //display info
     city.innerHTML = `${weather.city}`;
     icon.innerHTML = `<img src="icons/${weather.icon}.png">`;
     temp.innerHTML = `${weather.temp}°F`;
     desc.innerHTML = `${weather.desc}`;
-    minMax.innerHTML = `H:${weather.tempMin}° <span>-</span> L:${weather.tempMax}°`;
+    date.innerHTML = `${today}`;
+    minMax.innerHTML = `H:${weather.tempMin}° <span>-</span> L:${weather.tempMax}°F`;
 };
+
 
 
 
